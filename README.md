5b66dc9b030014b8ac9ecd63d671d3748164e787
Saul <s.shanabrook@gmail.com>
2014-03-11 16:01:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# `genetic-symbolicregression`
+
+Framework for 
+[genetic](http://www.github.com/saulshanabrook/genetic)
+that does data fitting using symblic regression through GP
+
+## License
+
+Copyright © 2014 Saul Shanabrook
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject genetic-symbolicregression "0.1.0-SNAPSHOT"
+  :description "Data fitting using symblic regression through GP"
+  :url "https://github.com/saulshanabrook/genetic-symbolicregression"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [genetic "0.1.0"]
+                 [roul "0.2.0"]]
+  :main ^:skip-aot genetic.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
